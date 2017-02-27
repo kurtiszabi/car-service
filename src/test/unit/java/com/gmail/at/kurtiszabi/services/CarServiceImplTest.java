cec1da892f9a400263424f3480b1212af5e2f00b
Kurti Szabolcs <kurtiszabi@gmail.com>
2017-02-27 08:19:47 +0100

    Add basic unit test cases to CarServiceImpl

--- src/test/unit/java/com/gmail/at/kurtiszabi/services/CarServiceImplTest.java
+++ src/test/unit/java/com/gmail/at/kurtiszabi/services/CarServiceImplTest.java
@@ -1,11 +1,23 @@
 package com.gmail.at.kurtiszabi.services;
 
-import org.mockito.Mock;
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.equalTo;
+import static org.mockito.Mockito.when;
 
+import java.util.List;
+
+import org.junit.Ignore;
+import org.junit.Test;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+
+import com.gmail.at.kurtiszabi.domain.Car;
+import com.gmail.at.kurtiszabi.exceptions.NotFoundException;
 import com.gmail.at.kurtiszabi.external.ExternalService;
 import com.gmail.at.kurtiszabi.repositories.CarRepository;
 import com.gmail.at.kurtiszabi.repositories.CarReservationRepository;
 import com.gmail.at.kurtiszabi.test.AbstractUnitTestBase;
+import com.google.common.collect.Lists;
 
 public class CarServiceImplTest extends AbstractUnitTestBase<CarServiceImpl> {
 
@@ -18,11 +30,38 @@ public class CarServiceImplTest extends AbstractUnitTestBase<CarServiceImpl> {
   @Mock
   private ExternalService extenalService;
 
+  @Mock
+  private Car car;
+
+  private List<Car> cars = Lists.newArrayList(car);
+
   @Override
   public CarServiceImpl createInstance() {
     return new CarServiceImpl(carRepository, carReservationRepository, extenalService);
   }
 
+  @Test
+  public void testGettingAllCars() {
+    when(carRepository.findAll()).thenReturn(cars);
+    List<Car> list = classUnderTest.getCars();
+    assertThat(list, equalTo(this.cars));
+  }
 
+  @Test
+  public void testGettingASingleCar() {
+    when(carRepository.findById(Mockito.anyLong())).thenReturn(car);
+    Car single = classUnderTest.getCar(1L);
+    assertThat(single, equalTo(car));
+  }
 
+  @Test(expected = NotFoundException.class)
+  public void testGettingANonExistentCar() {
+    classUnderTest.getCar(1L);
+  }
+
+  @Test
+  @Ignore("Until implemented")
+  public void testReservation() {
+    // TODO implement test case
+  }
 }
